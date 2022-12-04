0880dda03ac4d9bb2b2942a7f5380daccd20b600
284889754@qq.com <qq94619941103>
2022-12-04 22:30:56 +0800

    Initial commit

---
+++ one/src/main/java/com/xiaodong/controller/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.xiaodong.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ */
+@RestController
+@RequestMapping("/hello")
+
+public class HelloWorld {
+
+    @RequestMapping("/helloworld")
+    public String helloworld(){
+        return "HelloWorld!";
+    }
+}
---
+++ one/src/main/java/com/xiaodong/util/Print.java
@@ -0,0 +1,7 @@
+package com.xiaodong.util;
+
+public class Print {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
